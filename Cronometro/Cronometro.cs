74a7d18f1318ae7937274e4bb96f5f94d85a808f
eduardonunespp <carlospn200406@gmail.com>
2023-08-09 20:20:00 -0300

    feat: finish StopWatch

--- Cronometro/Cronometro.cs
+++ Cronometro/Cronometro.cs
@@ -1,6 +1,76 @@
-namespace Cronometro;
 
-public class Cronometro
+using System.ComponentModel.Design;
+
+using System;
+using System.Threading;
+
+namespace StopWatch
 {
-    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s: 10 segundos");
+            Console.WriteLine("M = Minuto => 1m: 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar");
+            
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+
+            if (type == 0)
+            {
+                System.Environment.Exit(0);
+            }
+            
+            PreStart(time * multiplier);
+            
+            Console.WriteLine(time);
+        }
+        
+        static void Start(int time)
+        {
+            int Time = time;
+            int currentTime = 0;
+
+            while (currentTime != Time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            
+            Console.Clear();
+            Console.WriteLine("StopWatch");
+            Thread.Sleep(2500);
+            Menu();
+        }
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+            
+            Start(time);
+        }
+    }
 }
+
